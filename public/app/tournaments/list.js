b88dc0450294607597023eca7101aad57a4b052d
Josh Krebs <joshuadenniskrebs@gmail.com>
2014-06-09 21:38:16 -0500

    some angular garbage'

--- public/app/tournaments/list.js
+++ public/app/tournaments/list.js
@@ -1,4 +1,5 @@
 angular.module('bassmonger')
     .controller('tournaments.list',['$scope',function($scope){
         $scope.title = 'Hello WOrld';
+        $scope.tournies =[{lake:'White Bear',name:'Test Tourney'},{lake:'Bald Eagle', name:'Test Tourney 2'}];
     }]);

13f0af542db7e75d08c75d20501c9e6872ca1041
Josh Krebs <joshuadenniskrebs@gmail.com>
2014-06-09 21:12:07 -0500

    adding test api method to test mongoose

--- src/api/v1.js
+++ src/api/v1.js
@@ -2,17 +2,16 @@ var path = require('path');
 var request = require('request');
 var _ = require('lodash');
 var mongoose = require('mongoose');
+var Tournament = mongoose.model('Tournament');
 
 exports.addRoutes = function(app) {
     app.get('/api/test',function(req,res){
-        mongoose.connect(ENV['MONGOHQ_URL']);
-        var Tournament = mongoose.model('Tournament',{date: Date,name:String,lake:String});
-        var tourney = new Tournament({date:new Date(),name:'testTourney', lake:'White Bear'});
+        var tourney = new Tournament({lake:'white bear',name:'testTourney'});
         tourney.save(function(error){
             if(error){
                 console.log(error);
             }
-        });
+        })
        res.send(tourney);
     });
 };
